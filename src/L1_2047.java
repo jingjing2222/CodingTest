c7f75006706f72dfc7eb13e534b6435b0d62572c
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2024-10-29 22:31:01 +0900

    문자열 입력 -> 대문자 변환 -> 출력

---
+++ src/L1_2047.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class L1_2047 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine().toUpperCase();
+        System.out.println(s);
+    }
+}
