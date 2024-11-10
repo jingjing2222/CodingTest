aae77451dff3a4159449f27d75545da4136d9c20
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2024-11-10 10:00:15 +0900

    소수점을 잘 확인하는 문제

---
+++ src/D3_11387.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class D3_11387 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int test_case=sc.nextInt();
+        for(int t=1; t<=test_case; t++){
+            long d=sc.nextInt();
+            long l=sc.nextInt();
+            long n=sc.nextInt();
+            long total_damage=0;
+            for(int i=0; i<n; i++){
+                total_damage+=d+(d*l*i)/100);
+            }
+            System.out.printf("#%d %d\n",t,total_damage);
+        }
+    }
+}
